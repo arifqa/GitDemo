f98d2c31d38aaa8521822bc5be0d368c911d66d1
-owner- <syedarifhussain98@gmail.com>
2020-06-18 13:59:19 -0500

    my first commit

---
+++ masjidquba/Mavenjava/AppiumTest.java
@@ -0,0 +1,24 @@
+package masjidquba.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	
+	@Test
+	public void NativeAppAndriod()
+	{
+		System.out.println("NavtiveAppAndriod");
+	}
+	
+	@
+	Test
+	public void IOSApps()
+	{
+	System.out.print("IOSApps");	
+		
+	}
+
+
+
+}
---
+++ src/main/java/masjidquba/Mavenjava/RESTAPITest.java
@@ -0,0 +1,23 @@
+package src.main.java.masjidquba.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	//testing xml file from Maven
+	// how to execute all test from test folder using -mvn test
+	@Test
+	public void postJira()
+	{
+		System.out.println("post jira");
+	}
+	
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("delete twitter");
+	}
+
+
+}
